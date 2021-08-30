f942277635b0adbfb22708d9807bf5082272b0f1
erianvc <erianvc.dev@gmail.com>
2021-08-30 16:54:32 -0500

    feat: agrega estilos y variables globales SCSS
    Refs #9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./styles.scss";
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
       HOLA BLUEBERRY!!
+
+      <Button />
     </div>
   );
 }
