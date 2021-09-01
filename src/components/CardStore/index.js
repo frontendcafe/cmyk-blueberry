721d81394690279425cbd8a3203c3af78907f6c3
unknown <cabreraceleste672@gmail.com>
2021-09-01 20:04:04 -0300

    Se creo el componente para la bodega y está adaptado a la vista de escritorio (tanto con fondo transparente como versión color), falta fijarse la vista mobile y ver las variables del sass

---
+++ src/components/CardStore/index.js
@@ -0,0 +1,25 @@
+import "./styles.scss";
+import grapeVector from "../../assets/img/grapeVector.svg"
+import homeVector from "../../assets/img/homeVector.svg"
+
+const CardStore = ({isTransparent, title, body}) => {
+    console.log(isTransparent);
+
+    return (
+        <div className="card">
+            <div className={`rectangle ${isTransparent ? "transparent-card" : "color-card"}`}>
+                <div className="div-vector">
+                    <img alt="" className="vector" src={isTransparent ? grapeVector : homeVector}></img>
+                </div>
+                <div className="title-div">
+                    <h3 className={isTransparent ? "transparent" : "color"}>{title}</h3>
+                </div>
+                <div className="body-div">
+                    <p className={isTransparent ? "transparent" : "color"}>{body}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardStore;
