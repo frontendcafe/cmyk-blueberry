3d93b0821a998cee592ea6cbd800cbea452fe41e
unknown <cabreraceleste672@gmail.com>
2021-09-03 00:32:31 -0300

    Eliminé console.log y pasé prettier

--- src/components/CardStore/index.js
+++ src/components/CardStore/index.js
@@ -1,25 +1,27 @@
 import "./styles.scss";
-import grapeVector from "../../assets/img/grapeVector.svg"
-import homeVector from "../../assets/img/homeVector.svg"
+import grapeVector from "../../assets/img/grapeVector.svg";
+import homeVector from "../../assets/img/homeVector.svg";
 
-const CardStore = ({isTransparent, title, body}) => {
-    console.log(isTransparent);
-
-    return (
-        <div className="card">
-            <div className={`rectangle ${isTransparent ? "transparent-card" : "color-card"}`}>
-                <div className="div-vector">
-                    <img alt="" className="vector" src={isTransparent ? grapeVector : homeVector}></img>
-                </div>
-                <div className="title-div">
-                    <h3 className={isTransparent ? "transparent" : "color"}>{title}</h3>
-                </div>
-                <div className="body-div">
-                    <p className={isTransparent ? "transparent" : "color"}>{body}</p>
-                </div>
-            </div>
+const CardStore = ({ isTransparent, title, body }) => {
+  return (
+    <div className="card">
+      <div className={`rectangle ${isTransparent ? "transparent-card" : "color-card"}`}>
+        <div className="div-vector">
+          <img
+            alt=""
+            className="vector"
+            src={isTransparent ? grapeVector : homeVector}
+          ></img>
         </div>
-    )
-}
+        <div className="title-div">
+          <h3 className={isTransparent ? "transparent" : "color"}>{title}</h3>
+        </div>
+        <div className="body-div">
+          <p className={isTransparent ? "transparent" : "color"}>{body}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CardStore;
