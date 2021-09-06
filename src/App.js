fc3e779333922389a06fdd5a22899cda27593dd8
Micaela2016 <miqa.orellano@gmail.com>
2021-09-06 15:54:03 -0300

    Cambio en App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Button from './components/Button'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      HOLA BLUEBERRY!!
-      <Button />
       <Navbar/>
     </div>
   );
