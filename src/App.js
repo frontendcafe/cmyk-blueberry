09d03d2b4f5ce8703f3fa580bb8ec4bb798f1095
Leonardo Coronel <leomcoronel99@gmail.com>
2021-09-05 21:22:09 -0300

    Commit cards-mobile de la sección info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import Button from './components/Button'
+import CardMobile from './components/Cards-mobile/index';
+import { cards } from './components/Cards-mobile/data';
+import logo from './assets/logo_barril.svg';
+import img from './assets/barriles.png';
+
 function App() {
   return (
     <div className="App">
+      HOLA BLUEBERRY!!
+
+      <Button />
+
+      {cards.map((card) => {
+        return <CardMobile title={card.title} description={card.description} img={img} logo={logo} />
+      })}
     </div>
   );
 }
