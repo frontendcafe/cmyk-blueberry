5f503f38135061376335608511d470732d6d491a
Giselle <gisellechaumont@gmail.com>
2021-09-06 20:20:27 -0300

    button listo

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,48 @@
 import './styles.scss';
 
-function Button() {
+
+export const Button = (props) => {
   return (
-    <button className="button">
-      Botón de prueba
-    </button>
-  );
+
+        <button id={props.variant}>
+          {props.text}
+        </button>
+    
+  )
+
 }
+
+
+
+// function Button() {
+//   return (
+//     <>
+
+
+//     <button className="primary">
+//       Contactanos
+//     </button>
+
+//     <button className="secondary">
+//     Quiero saber mas
+//     </button>
+    
+
+//         <button className="light">
+//        Hola
+//     </button>
+
+//     <button className="whit">
+//     CONOCE LO NUEVO
+//     </button>
+
+//     <button className="info">
+//     Más información   > 
+//     </button>
+
+//     </>
+
+//   );
+// }
 
 export default Button;
