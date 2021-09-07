8536762f00587e468f4ddbf5343bbce86834a22d
Giselle <gisellechaumont@gmail.com>
2021-09-07 10:58:46 -0300

    di fomato con prettier

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,18 +1,7 @@
-import './styles.scss';
-
+import "./styles.scss";
 
 export const Button = (props) => {
-  return (
-
-        <button id={props.variant}>
-          {props.text}
-        </button>
-    
-  )
-
-}
-
-
-
+  return <button id={props.variant}>{props.text}</button>;
+};
 
 export default Button;
