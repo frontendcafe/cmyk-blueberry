d520d4e5208317f17f1482a5bef145bf2d9b4e88
erianvc <erianvc.dev@gmail.com>
2021-09-06 21:04:39 -0500

    refactor: mueve imágenes a la carpeta `assets`
    Refs #10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Button from './components/Button'
 import CardDesktop from './components/CardDesktop'
+import BarrilesImg from './assets/barriles.png'
+import CopasImg from './assets/copas.png'
+import CultivoImg from './assets/cultivo.png'
 
 function App() {
   return (
@@ -16,13 +19,13 @@ function App() {
         paddingTop: '2rem',
         paddingBottom: '2rem',
       }}>
-        <CardDesktop title="Nuestras bodegas" icon="/images/barriles.png">
+        <CardDesktop title="Nuestras bodegas" icon={BarrilesImg}>
           Ubicada en Mendoza, Bodegas Wineberry se encuentra en un mágico lugar.
           Aquí las vides reciben permanente sol, viento suave y seco, y abundante agua,
           que baja de las montañas y se combina con los suelos pedregosos, logrando vinos de alta calidad, frutados y redondos.
         </CardDesktop>
 
-        <CardDesktop type="primary" title="Nuestras proceso" icon="/images/copas.png" cover="/images/cultivo.png">
+        <CardDesktop type="primary" title="Nuestras proceso" icon={CopasImg} cover={CultivoImg}>
           Todo el proceso, desde la plantación de viñas, cosecha, elaboración, fraccionamiento, distribución y comercialización,
           es cuidadosamente controlado con un equipamiento tecnológico de última generación, lográndose así resultados por demás satisfactorios.
         </CardDesktop>
