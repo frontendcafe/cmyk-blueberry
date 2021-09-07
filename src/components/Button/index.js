28a534d1e577973758b42f36c95f6bd32985a58c
Giselle <gisellechaumont@gmail.com>
2021-09-07 16:26:12 -0300

    arregle las recomendaciones

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
 import "./styles.scss";
 
 export const Button = (props) => {
-  return <button id={props.variant}>{props.text}</button>;
+  return <button className={props.variant}>{props.text}</button>;
 };
 
 export default Button;
