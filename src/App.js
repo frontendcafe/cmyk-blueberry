3d464e874b8bedf16f5dd0a244b496208166bb65
Micaela2016 <miqa.orellano@gmail.com>
2021-09-11 13:54:33 -0300

    Vista mobile casi lista

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import WineDetail from "./components/WineDetail";
+
 function App() {
   return (
     <div className="App">
+      <WineDetail/>
     </div>
   );
 }
--- src/components/WineDetail/index.js
+++ src/components/WineDetail/index.js
@@ -5,46 +5,49 @@ import aromaIcon from '../../assets/aroma-icon.svg';
 import saborIcon from '../../assets/sabor-icon.svg';
 import prensa from '../../assets/prensa.png'
 
+//PRUEBA
+import vino from '../../assets/wine-mattwe.png';
+
 
 const WineDetail = () => {
     return (
         <article className="detail">
             <div className="detail__img">
-                <img alt="vino"/>
+                <img src={vino} alt="vino"/>                                                
             </div>
-            <section className="detail__sectionPrincipal">
-                <h1 className="detail__section-title"></h1>
-                <p className="detail__section-text"></p>
+            <section className="detail__section first">
+                <h1 className="detail__section-title--primary">MATTWE CABERNET 2010</h1>
+                <p className="detail__section-text">Creamos una línea de vinos únicos y elegantes, en donde cada uno es fruto del trabajo en un viñedo especial, por su edad y su manejo. Elaborado con uvas provenientes de nuestros viñedos centenarios plantados en 1906 y 1930.</p>
             </section>
-            <section className="detail__section">
-                <h2 className="detail__section-title--color">COMPOSICION VARIETAL</h2>
-                <p className="detail__section-text"></p>
+            <section className="detail__section second">
+                <h2 className="detail__section-title--secondary">COMPOSICION VARIETAL</h2>
+                <p className="detail__section-text">Malbec (Gualtallary) 56%, Cabernet Sauvignon (Gualtallary) 12.5% y Cabernet Sauvignon (Perdriel, Luján de Cuyo) 31.5%.</p>
             </section>
                 
-            <section className="detail__section">
-                <h2 className="detail__section-title--color">NOTAS DE CATA</h2>
-                <div>
+            <section className="detail__section third">
+                <h2 className="detail__section-title--secondary">NOTAS DE CATA</h2>
+                <div className="detail__section-item">
                     <img src={colorIcon} alt="Icon color"></img>
-                    <h3>Color</h3>
-                    <p></p>
+                    <h3>Color:</h3>
+                    <p>Rojos con reflejos bordo</p>
                 </div>
-                <div>
+                <div className="detail__section-item">
                     <img src={aromaIcon} alt="Icon aroma"></img>
-                    <h3>Aroma</h3>
-                    <p></p>
+                    <h3>Aroma:</h3>
+                    <p>Predominan los aromas de frutos rojos maduros y mermeladas, combinados con el suave aporte aromático del roble, vainilla chocolate y algunos ahumados.</p>
                 </div>
-                <div>
+                <div className="detail__section-item">
                     <img src={saborIcon} alt="Icon sabor"></img>
-                    <h3>Sabor</h3>
-                    <p></p>
+                    <h3>Sabor:</h3>
+                    <p>Boca elegante en la que se vuelve imperceptible el paso por madera.</p>
                 </div>
             </section>
-            <section className="detail__section">
-                <h2 className="detail__section-title--color">MARIDAJE SUGERIDO</h2>
-                <p></p>
+            <section className="detail__section fourth">
+                <h2 className="detail__section-title--secondary">MARIDAJE SUGERIDO</h2>
+                <p className="detail__section-text">Acompaña muy bien carnes rojas a la parrilla, quesos duros y pastas con salsas blancas así como de tomate o trufas. También es ideal para platos principales, como guisos y carnes de caza o verduras, como papas rellenas, cabutia al horno y pimientos.</p>
             </section>
-            <section>
-                <button>Volver</button>
+            <section className="detail__section fifth">
+                <button className="detail__section-button">Volver</button>
                 <img src={prensa} alt="Prensa de uvas azul"/>
             </section>
         </article>
