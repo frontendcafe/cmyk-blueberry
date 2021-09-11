b1c61bf2f477fb34747f3baf35a8090c34252c33
Micaela2016 <miqa.orellano@gmail.com>
2021-09-11 15:26:30 -0300

    Ultimos arreglos y agrego de button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Button from '../Button'
 import logo from '../../assets/logo.png';
 import menuIcon from '../../assets/menu-icon.svg';
 import closeIcon from '../../assets/close-icon.svg';
@@ -11,9 +12,9 @@ import searchIcon from '../../assets/search-icon.svg';
 import './styles.scss';
 
 const Navbar = () => {
-    const [isOpen, setClick] = useState(false);
-    const handleClick = () => setClick(!isOpen);
-    const closeMobileMenu = () => setClick(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => setIsOpen(!isOpen);
+    const closeMobileMenu = () => setIsOpen(false);
     const scrollToTop= () => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
@@ -24,44 +25,43 @@ const Navbar = () => {
 
             <div className="navbar__mobile" onClick={handleClick}>
                 {isOpen ? (
-                    <img className="navbar__mobile-icon" alt="Icon cierre" src={closeIcon}/>
+                    <img className="navbar__mobile--icon" alt="Icon cierre" src={closeIcon}/>
                 ) : (
                     <img alt="Icon menu" src={menuIcon}/>
             )}
             </div>
 
             <div className="navbar__logo">
-                <a className="navbar__logo-link" onClick= {scrollToTop} href='/'><img className='navbar__logo-img' alt="Logo Wineberry" src={logo}/></a>
+                <a className="navbar__logo--link" onClick= {scrollToTop} href='/'><img className='navbar__logo--img' alt="Logo Wineberry" src={logo}/></a>
             </div>
 
-            <div className={isOpen ? "navbar__menu active" : "navbar__menu"}>
+            <div className={isOpen ? "navbar__menu navbar__menu--active" : "navbar__menu"}>
                 <ul>
-                    <li className="navbar__menu-item" onClick={closeMobileMenu}>
+                    <li className="navbar__menu--item" onClick={closeMobileMenu}>
                         <img className="hidden" alt="Icon nuestra bodega" src={bodegaIcon}/>
                         <a href="#nuestrasBodegas">Bodega Wineberry</a>
                     </li>
-                    <li className="navbar__menu-item" onClick={closeMobileMenu}>
+                    <li className="navbar__menu--item" onClick={closeMobileMenu}>
                         <img className="hidden"  alt="Icon nuestros vinos" src={vinosIcon}/>
                         <a href="#nuestrosVinos">Nuestros vinos</a>
                     </li>
-                    <li className="navbar__menu-item" onClick={closeMobileMenu}>
+                    <li className="navbar__menu--item" onClick={closeMobileMenu}>
                         <img className="hidden"  alt="Icon galeria" src={galeriaIcon}/>
                         <a href="#galeria">Galeria</a>
                     </li>
-                    <li className="navbar__menu-item hidden" onClick={closeMobileMenu}>
+                    <li className="navbar__menu--item hidden" onClick={closeMobileMenu}>
                         <img className="hidden" alt="Icon sobre nosotros" src={nosotrosIcon}/>
                         <a className="hidden" href="#sobreNosotros">Sobre nosotros</a>
                     </li>
-                    <li className="navbar__menu-item hidden" onClick={closeMobileMenu}>
+                    <li className="navbar__menu--item hidden" onClick={closeMobileMenu}>
                         <img className="hidden" alt="Icon contacto" src={contactoIcon}/>
                         <a className="hidden" href="#contacto">Contactanos</a>
                     </li>
                 </ul>
             </div>
-
-            <button className="navbar__button">
-                <a className="navbar__button-link" href="#contacto">Contactanos</a>
-            </button>
+            <a className="navbar__contact" href="#contacto"> 
+                <Button variant="primary" text="Contactanos" clases="navbar__contact--btn" ></Button>
+            </a>
 
             <div className="navbar__search">
                 <img src={searchIcon} alt="Icon busqueda"/>
