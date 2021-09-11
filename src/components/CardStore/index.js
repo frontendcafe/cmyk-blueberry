5765aabdec215449fc83a749ef548562bd71b7e0
unknown <cabreraceleste672@gmail.com>
2021-09-11 14:46:58 -0300

    Agregue bem a estilos de la card

--- src/components/CardStore/index.js
+++ src/components/CardStore/index.js
@@ -3,27 +3,27 @@ import grapeVector from "../../assets/grapeVector.svg";
 import homeVector from "../../assets/homeVector.svg";
 import heartVector from "../../assets/heartVector.svg";
 
-const CardStore = ({ isTransparent, title, body }) => {
+const CardStore = ({ variant, title, body }) => {
   return (
-    <div className="card">
-      <div className={`rectangle ${isTransparent ? "transparent-card" : "color-card"}`}>
-        <div className="div-vector">
+    <div className={`card  card--${variant}`}>
+      <div className={`card__container`}>
+        <div className="vector">
           <img
             alt=""
-            className="vector"
-            src={isTransparent ? grapeVector : homeVector}
+            className="vector__element"
+            src={variant === "transparent" ? grapeVector : homeVector}
           ></img>
           <img
             alt=""
-            className="heart-vector"
-            src={isTransparent ? "" : heartVector}
+            className="vector__heart"
+            src={variant === "transparent" ? "" : heartVector}
           ></img>
         </div>
-        <div className="title-div">
-          <h3 className={` h3 ${isTransparent ? "transparent" : "color"}`}>{title}</h3>
+        <div className={`title title--${variant}`}>
+          <h3 className="title__text">{title}</h3>
         </div>
-        <div className="body-div">
-          <p className={isTransparent ? "transparent" : "color"}>{body}</p>
+        <div className={`body body--${variant}`}>
+          <p className="body__text">{body}</p>
         </div>
       </div>
     </div>
--- src/views/sucursales/index.js
+++ src/views/sucursales/index.js
@@ -7,12 +7,12 @@ const Sucursales = () => {
         <div id="sucursales-div">
             <h1 className="title">NUESTRAS SUCURSALES</h1>
             <div id="cards">
-                <CardStore title="Bodega wineberry
+                <CardStore variant="color" title="Bodega wineberry
                 -Mendoza-" body="Ozamis Norte 375 · General Gutiérrez ·
                 M5511APG · Maipú · Mendoza
                 +54 9 261 5948023
                 infomendoza@bodegaswineberry.com.ar"></CardStore>
-                <CardStore isTransparent title="Bodega wineberry
+                <CardStore variant="transparent" title="Bodega wineberry
                 -Buenos Aires-" body="Godoy Cruz 2025 · C1414CYP ·
                 Capital Federal · Buenos Aires
                 +54 9 11 26062011
