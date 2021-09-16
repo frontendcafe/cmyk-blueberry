484710b4e470c102f4174020356614718d2cc63d
unknown <cabreraceleste672@gmail.com>
2021-09-15 22:51:18 -0300

    Descargué librería react-router-dom, hice enrutado en app.js y cree vistas de 'sobre-nosotros' y 'lo-nuevo'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import Navbar from "./components/Navbar";
 import Sucursales from "./views/sucursales/index.js"
 import Gallery from './components/Gallery/index';
-
+import AboutUS from "./views/aboutUS";
+import News from "./views/news";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+ 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Sucursales />
-      <Gallery />
+      <Router>
+        <Navbar></Navbar>
+          <Switch>
+            <Route path="/home" exact>
+              <Sucursales></Sucursales>
+              <Gallery></Gallery>
+            </Route>
+            <Route path="/sobre-nosotros" exact>
+              <AboutUS></AboutUS>
+            </Route>
+            <Route path="/lo-nuevo" exact>
+              <News></News>
+            </Route>
+            <Route path="/" exact>
+              <Sucursales></Sucursales>
+              <Gallery></Gallery>
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
