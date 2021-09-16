f949dd4c94e9f144c1000bf239ea738c51b85925
Micaela2016 <miqa.orellano@gmail.com>
2021-09-16 11:48:25 -0300

    Agrego props, nuevos iconos, arreglos varios

--- src/components/WineDetail/data.js
+++ src/components/WineDetail/data.js
@@ -1,29 +1,42 @@
+import wineMattwe from '../../assets/wine-mattwe.png';
+import wineNisoma from '../../assets/wine-nisoma.png';
+import wineRedwine from '../../assets/wine-redwine.png'
+
+
+
 export const wines = [
     {
-        "title": "MATTWE CABERNET 2010",
+        "title": "MATTWE - Since 2010",
         "description": "Creamos una línea de vinos únicos y elegantes, en donde cada uno es fruto del trabajo en un viñedo especial, por su edad y su manejo. Elaborado con uvas provenientes de nuestros viñedos centenarios plantados en 1906 y 1930.",
         "composition": "100% Cabernet.",
         "color": "Rojos con reflejos bordo",
         "aroma": "Predominan los aromas de frutos rojos maduros y mermeladas, combinados con el suave aporte aromático del roble, vainilla chocolate y algunos ahumados.",
         "sabor": "Boca elegante en la que se vuelve imperceptible el paso por madera.",
-        "maridaje": "Acompaña muy bien carnes rojas a la parrilla, quesos duros y pastas con salsas blancas así como de tomate o trufas. También es ideal para platos principales, como guisos y carnes de caza o verduras, como papas rellenas, cabutia al horno y pimientos."
+        "maridaje": "Acompaña muy bien carnes rojas a la parrilla, quesos duros y pastas con salsas blancas así como de tomate o trufas. También es ideal para platos principales, como guisos y carnes de caza o verduras, como papas rellenas, cabutia al horno y pimientos.",
+        "background" :"secondary",
+        "img" : wineMattwe
+    
     }, 
     {
-        "title": "NISOMA CABERNET SAUVIGNON 2018",
+        "title": "NISOMA - Since 2018",
         "description": "Es el vino ícono de Bodega Wineberry. Busca complejidad y elegancia. Se elabora de uvas de bajos rendimientos, con largas maceraciones. Su crianza dura dos años en barricas nuevas de roble francés y un año de estiba en botella antes de salir al mercado.",
         "composition": "Malbec (Gualtallary) 56%, Cabernet Sauvignon (Gualtallary) 12.5% y Cabernet Sauvignon (Perdriel, Luján de Cuyo) 31.5%.",
         "color": "Rojo oscuro muy intenso.",
         "aroma": "Gran complejidad aromática, frutos rojos y negros maduros y en confituras, especias y roble muy elegante bien integrado.",
         "sabor": "Entrada en boca dulce, taninos de textura fina, muy estructurado y persisteznte.",
-        "maridaje": "Ideal para cenas especiales, donde el plato principal sea carne, que puede ser vacuna, de pato o cordero. También acompaña bien a un cerdo braseado o un chivo hecho a los fuegos."
+        "maridaje": "Ideal para cenas especiales, donde el plato principal sea carne, que puede ser vacuna, de pato o cordero. También acompaña bien a un cerdo braseado o un chivo hecho a los fuegos.",
+        "background" :"primary",
+        "img" : wineNisoma
     },
     {
-        "title": "RED WINE 2019",
+        "title": "Red Wine - Since 2003",
         "description": "Es una línea de vinos de alta gama elaborados en pequeñas cantidades, resultado de largos periodos de investigación y reconocimiento de los mejores terruños dentro de nuestras fincas. Cada ejemplar es el fiel reflejo de un viñedo especial ubicado en una microzona de la región de Cuyo seleccionada por sus características de calidad.",
         "composition": "100% Malbec.",
         "color": "Rojo con reflejos bordó.",
         "aroma": "Predominan los aromas de frutos rojos maduros y mermeladas, combinados con el suave aporte aromático del roble, vainilla chocolate y algunos ahumados.",
         "sabor": "Buena concentración y untuosidad, con taninos sedosos, típico del malbec argentino.",
-        "maridaje": "Ideal para acompañar con carnes rojas asadas o pastas con salsas rojas."
+        "maridaje": "Ideal para acompañar con carnes rojas asadas o pastas con salsas rojas.",
+        "background" :"colorCard",
+        "img" : wineRedwine
     }
 ]
--- src/components/WineDetail/index.js
+++ src/components/WineDetail/index.js
@@ -1,53 +1,58 @@
 import React from 'react';
 import "./styles.scss";
-import colorIcon from '../../assets/color-icon.svg';
-import aromaIcon from '../../assets/aroma-icon.svg';
-import saborIcon from '../../assets/sabor-icon.svg';
+import colorIconM from '../../assets/color-icon.svg';
+import aromaIconM from '../../assets/aroma-icon.svg';
+import saborIconM from '../../assets/sabor-icon.svg';
+import colorIconD from '../../assets/color-iconD.svg';
+import aromaIconD from '../../assets/aroma-iconD.svg';
+import saborIconD from '../../assets/sabor-iconD.svg';
 import prensa from '../../assets/prensa.png'
 
-//PRUEBA
-import vino from '../../assets/wine-mattwe.png';
-
-
-const WineDetail = () => {
+const WineDetail = ({title, description, composition, color, aroma, sabor, maridaje, background, img}) => {
+    const goBack= () => {
+        window.history.back();
+    }
     return (
-        <section className="detail">
+        <section className={`detail detail--${background}`}>
             <div className="detail__img">
-                <img src={vino} alt="vino"/>                                                
+                <img src={img} alt="vino"/>                                                
             </div>
-            <section className="detail__section first">
-                <h1 className="detail__section-title detail__section-title--primary ">MATTWE CABERNET 2010</h1>
-                <p className="detail__section-text">Creamos una línea de vinos únicos y elegantes, en donde cada uno es fruto del trabajo en un viñedo especial, por su edad y su manejo. Elaborado con uvas provenientes de nuestros viñedos centenarios plantados en 1906 y 1930.</p>
+            <section className="detail__section detail__section--first">
+                <h1 className="detail__section-title detail__section-title--primary ">{title}</h1>
+                <p className="detail__section-text">{description}</p>
             </section>
-            <section className="detail__section second">
+            <section className="detail__section detail__section--second">
                 <h2 className="detail__section-title detail__section-title--secondary">COMPOSICION VARIETAL</h2>
-                <p className="detail__section-text">Malbec (Gualtallary) 56%, Cabernet Sauvignon (Gualtallary) 12.5% y Cabernet Sauvignon (Perdriel, Luján de Cuyo) 31.5%.</p>
+                <p className="detail__section-text">{composition}</p>
             </section>
                 
-            <section className="detail__section third">
+            <section className="detail__section detail__section--third">
                 <h2 className="detail__section-title detail__section-title--secondary">NOTAS DE CATA</h2>
                 <div className="detail__section-item">
-                    <img src={colorIcon} alt="Icon color"></img>
+                    <img className="icon--desktop" src={colorIconD} alt="Icon sabor"></img>
+                    <img className="icon--mobile" src={colorIconM} alt="Icon color"></img>
                     <h3>Color:</h3>
-                    <p>Rojos con reflejos bordo</p>
+                    <p>{color}</p>
                 </div>
                 <div className="detail__section-item">
-                    <img src={aromaIcon} alt="Icon aroma"></img>
+                    <img className="icon--desktop" src={aromaIconD} alt="Icon sabor"></img>
+                    <img className="icon--mobile" src={aromaIconM} alt="Icon aroma"></img>
                     <h3>Aroma:</h3>
-                    <p>Predominan los aromas de frutos rojos maduros y mermeladas, combinados con el suave aporte aromático del roble, vainilla chocolate y algunos ahumados.</p>
+                    <p>{aroma}</p>
                 </div>
                 <div className="detail__section-item">
-                    <img src={saborIcon} alt="Icon sabor"></img>
+                    <img className="icon--desktop" src={saborIconD} alt="Icon sabor"></img>
+                    <img className="icon--mobile" src={saborIconM} alt="Icon sabor"></img>
                     <h3>Sabor:</h3>
-                    <p>Boca elegante en la que se vuelve imperceptible el paso por madera.</p>
+                    <p>{sabor}</p>
                 </div>
             </section>
-            <section className="detail__section fourth">
+            <section className="detail__section detail__section--fourth">
                 <h2 className="detail__section-title detail__section-title--secondary">MARIDAJE SUGERIDO</h2>
-                <p className="detail__section-text">Acompaña muy bien carnes rojas a la parrilla, quesos duros y pastas con salsas blancas así como de tomate o trufas. También es ideal para platos principales, como guisos y carnes de caza o verduras, como papas rellenas, cabutia al horno y pimientos.</p>
+                <p className="detail__section-text">{maridaje}</p>
             </section>
-            <section className="detail__section fifth">
-                <button className="detail__section-button">Volver</button>
+            <section className="detail__section detail__section--fifth">
+                <button onClick={goBack} className="detail__section-button">Volver</button>
                 <img className="detail__section-img" src={prensa} alt="Prensa de uvas azul"/>
             </section>
         </section>
