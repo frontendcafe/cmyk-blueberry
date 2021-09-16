d5000b898db22ae68418437f187074fb55f7c13a
Marc <marc@taxscouts.com>
2021-09-16 20:10:36 +0200

    Uso de Link en lugar de anchors en la Navbar Añadido useScrollToHash para hacer scroll a la sección activa tras la navegación

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
-import Button from '../Button'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Button';
 import logo from '../../assets/logo.png';
 import menuIcon from '../../assets/menu-icon.svg';
 import closeIcon from '../../assets/close-icon.svg';
@@ -12,62 +13,107 @@ import searchIcon from '../../assets/search-icon.svg';
 import './styles.scss';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClick = () => setIsOpen(!isOpen);
-    const closeMobileMenu = () => setIsOpen(false);
-    const scrollToTop= () => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  const closeMobileMenu = () => setIsOpen(false);
+  const scrollToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
 
-    return (
-        <nav className="navbar">
+  return (
+    <nav className="navbar">
+      <div className="navbar__mobile" onClick={handleClick}>
+        {isOpen ? (
+          <img
+            className="navbar__mobile-icon"
+            alt="Icon cierre"
+            src={closeIcon}
+          />
+        ) : (
+          <img alt="Icon menu" src={menuIcon} />
+        )}
+      </div>
 
-            <div className="navbar__mobile" onClick={handleClick}>
-                {isOpen ? (
-                    <img className="navbar__mobile-icon" alt="Icon cierre" src={closeIcon}/>
-                ) : (
-                    <img alt="Icon menu" src={menuIcon}/>
-            )}
-            </div>
+      <div className="navbar__logo">
+        <Link className="navbar__logo-link" onClick={scrollToTop} to="/">
+          <img className="navbar__logo-img" alt="Logo Wineberry" src={logo} />
+        </Link>
+      </div>
 
-            <div className="navbar__logo">
-                <a className="navbar__logo-link" onClick= {scrollToTop} href='/'><img className='navbar__logo-img' alt="Logo Wineberry" src={logo}/></a>
-            </div>
+      <div
+        className={
+          isOpen ? 'navbar__menu navbar__menu--active' : 'navbar__menu'
+        }
+      >
+        <ul>
+          <li className="navbar__menu-item" onClick={closeMobileMenu}>
+            <img
+              className="hidden"
+              alt="Icon nuestra bodega"
+              src={bodegaIcon}
+            />
+            <Link
+              to={{
+                pathname: '/',
+                hash: '#nuestrasBodegas',
+              }}
+            >
+              Bodega Wineberry
+            </Link>
+          </li>
+          <li className="navbar__menu-item" onClick={closeMobileMenu}>
+            <img className="hidden" alt="Icon nuestros vinos" src={vinosIcon} />
+            <Link
+              to={{
+                pathname: '/',
+                hash: '#nuestrosVinos',
+              }}
+            >
+              Nuestros vinos
+            </Link>
+          </li>
+          <li className="navbar__menu-item" onClick={closeMobileMenu}>
+            <img className="hidden" alt="Icon galeria" src={galeriaIcon} />
+            <Link
+              to={{
+                pathname: '/',
+                hash: '#galeria',
+              }}
+            >
+              Galeria
+            </Link>
+          </li>
+          <li className="navbar__menu-item hidden" onClick={closeMobileMenu}>
+            <img
+              className="hidden"
+              alt="Icon sobre nosotros"
+              src={nosotrosIcon}
+            />
+            <Link className="hidden" to="#sobreNosotros">
+              Sobre nosotros
+            </Link>
+          </li>
+          <li className="navbar__menu-item hidden" onClick={closeMobileMenu}>
+            <img className="hidden" alt="Icon contacto" src={contactoIcon} />
+            <Link className="hidden" to="#contacto">
+              Contactanos
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Link className="navbar__contact" to="#contacto">
+        <Button
+          variant="primary"
+          text="Contactanos"
+          clases="navbar__contact-btn"
+        ></Button>
+      </Link>
 
-            <div className={isOpen ? "navbar__menu navbar__menu--active" : "navbar__menu"}>
-                <ul>
-                    <li className="navbar__menu-item" onClick={closeMobileMenu}>
-                        <img className="hidden" alt="Icon nuestra bodega" src={bodegaIcon}/>
-                        <a href="#nuestrasBodegas">Bodega Wineberry</a>
-                    </li>
-                    <li className="navbar__menu-item" onClick={closeMobileMenu}>
-                        <img className="hidden"  alt="Icon nuestros vinos" src={vinosIcon}/>
-                        <a href="#nuestrosVinos">Nuestros vinos</a>
-                    </li>
-                    <li className="navbar__menu-item" onClick={closeMobileMenu}>
-                        <img className="hidden"  alt="Icon galeria" src={galeriaIcon}/>
-                        <a href="#galeria">Galeria</a>
-                    </li>
-                    <li className="navbar__menu-item hidden" onClick={closeMobileMenu}>
-                        <img className="hidden" alt="Icon sobre nosotros" src={nosotrosIcon}/>
-                        <a className="hidden" href="#sobreNosotros">Sobre nosotros</a>
-                    </li>
-                    <li className="navbar__menu-item hidden" onClick={closeMobileMenu}>
-                        <img className="hidden" alt="Icon contacto" src={contactoIcon}/>
-                        <a className="hidden" href="#contacto">Contactanos</a>
-                    </li>
-                </ul>
-            </div>
-            <a className="navbar__contact" href="#contacto"> 
-                <Button variant="primary" text="Contactanos" clases="navbar__contact-btn" ></Button>
-            </a>
-
-            <div className="navbar__search">
-                <img src={searchIcon} alt="Icon busqueda"/>
-            </div>
-
-        </nav>
-    );
+      <div className="navbar__search">
+        <img src={searchIcon} alt="Icon busqueda" />
+      </div>
+    </nav>
+  );
 };
-    export default Navbar;
+export default Navbar;
