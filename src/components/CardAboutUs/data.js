67502b956e77e8062e0362ec556d64304f35b190
Ignacio Agustin Cernich <ignacio.cernich@est.fi.uncoma.edu.ar>
2021-09-20 19:44:48 -0300

    update

--- src/components/CardAboutUs/data.js
+++ src/components/CardAboutUs/data.js
@@ -1,5 +1,9 @@
+import  MariaSofiaTerragno  from "../../assets/MariaSofiaTerragno.png";
+import  Ana  from "../../assets/Ana.png";
+
 export const cards = [
   {
+    image: { MariaSofiaTerragno },
     name: "María Sofia Terragno",
     role: "Diseñadora UX",
     mail: "sofiaterragno@gmail.com",
@@ -11,6 +15,7 @@ export const cards = [
     ],
   },
   {
+    image: { Ana },
     name: "Ana",
     role: "Diseñadora UX",
     mail: "sofiaterragno@gmail.com",
--- src/components/CardAboutUs/index.js
+++ src/components/CardAboutUs/index.js
@@ -1,26 +1,38 @@
 import React from "react";
 import { cards } from "./data";
 import "./styles.scss";
+import { linkedin } from "../../assets/linkedin.png";
+import { github } from "../../assets/github.png";
 
 function CardAboutUs() {
-  return cards.map((card) => (
-    <div className="card-container">
-      <img className="card-container__image"></img>
-      <div className="card-container__box">
-        <h1> {card.name} </h1>
-        <h2> {card.role} </h2>
-        <p> {card.mail} </p>
-        <div className="card-container__box--icons">
-          <a href={card.links.linkedin}>
-            <img></img>
-          </a>
-          <a href={card.links.github}>
-            <img></img>
-          </a>
+  return (
+    <div className="container">
+      {cards.map((card) => (
+        <div className="card-container">
+          <img className="card-container__image" src={card.image}></img>
+          <div className="card-container__box">
+            <h1> {card.name} </h1>
+            <h2> {card.role} </h2>
+            <p> {card.mail} </p>
+            <div className="card-container__box__icons">
+              <a href={card.links.linkedin}>
+                <img
+                  className="card-container__box__icons--linkedin"
+                  src={linkedin}
+                ></img>
+              </a>
+              <a href={card.links.github}>
+                <img
+                  className="card-container__box__icons--github"
+                  src={github}
+                ></img>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
+      ;
     </div>
-  ));
+  );
 }
-
 export default CardAboutUs;
