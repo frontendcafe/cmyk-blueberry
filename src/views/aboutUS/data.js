fd9f91385cd5c77d127cee98637ef0a17d5cd90a
unknown <cabreraceleste672@gmail.com>
2021-09-21 22:33:35 -0300

    Agregué logo y texto

---
+++ src/views/aboutUS/data.js
@@ -0,0 +1,5 @@
+const data = {
+    text: "Wineberry surgió a traves del proyecto CMYK creada por Frontend Café donde impulsan el desarrollo de proyectos colaborativos realizados por miembros de la comunidad con el objetivo de ganar experiencia en un entorno profesional. Este proyecto es el resultado del trabajo en equipo, iteraciones y mucho esfuerzo. Somos un equipo de diseñadores y desarrolladores que busca demostrar todo su conocimiento y seguir aprendiendo dentro del mundo digital."
+}
+
+export default data;
--- src/views/aboutUS/index.js
+++ src/views/aboutUS/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import logo from "../../assets/logo.png"
+import "./style.scss"
+import data from "./data"
 
 const AboutUS = () => {
     return(
-
-        //Solo tienen este margin ahora para que se vea debajo del navbar y saber rápidamente si las
-        //rutas funcionan correctamente! Una vez se cree el correspondiente componente, borrar eso.
-        <div style={{marginTop:200}}>AboutUS works!</div>
+        <div className="mainDiv">
+            <div className="mainDiv__img">
+                <img src={logo}></img>
+            </div>
+            <div className="mainDiv__text">
+                <p className="text__content">"Wineberry surgió a traves del proyecto CMYK creada por Frontend Café
+                donde impulsan el desarrollo de proyectos colaborativos realizados por miembros de la comunidad con 
+                el objetivo de ganar experiencia en un entorno profesional. Este proyecto es el resultado del 
+                <b> trabajo en equipo, iteraciones y mucho esfuerzo.</b> </p>
+                <br/>
+                <p className="text__content"> Somos un equipo de diseñadores y desarrolladores
+                que busca demostrar todo su conocimiento y seguir aprendiendo dentro del mundo digital."</p>
+            </div>
+        </div>
     )
 }
 
